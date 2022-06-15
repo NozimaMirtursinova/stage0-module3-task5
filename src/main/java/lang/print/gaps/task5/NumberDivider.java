2e14e0edc005021d464809479ed42bbb6aedabce
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 16:29:08 +0300

    Fix POM.xml

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        int result = toBeDivided / 5;
+        System.out.println(result);
     }
+    divide(toBeDivided);
+    public static void(String[] args){
+        divide(10);
+    }
+
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        /*Implement a program that will convert minutes to seconds
+        and print it console. Implement program in the code snippet below:
+        where minutes - variable to be converted.*/
+        float seconds;
+        minutes = 10.0F;
+        seconds = minutes * 60;
+        System.out.println(seconds);
+    }
+    public static void main(String[] args){
+
     }
 }
